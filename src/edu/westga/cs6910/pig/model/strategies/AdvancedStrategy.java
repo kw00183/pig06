06b3e3dd037e96c73b5d5c286608281cb34dd73c
Kim Weible <kw00183@my.westga.edu>
2021-07-10 14:02:52 -0500

    AdvancedStrategy - add in javadoc comments and the method rollAgain

--- src/edu/westga/cs6910/pig/model/strategies/AdvancedStrategy.java
+++ src/edu/westga/cs6910/pig/model/strategies/AdvancedStrategy.java
@@ -7,6 +7,18 @@ package edu.westga.cs6910.pig.model.strategies;
  * @author Kim Weible
  * @version Summer 2021
  */
-public abstract class AdvancedStrategy implements PigStrategy {
+public class AdvancedStrategy implements PigStrategy {
 
+	/**
+	 * Method determines whether the player should roll again or not
+	 * 
+	 * @param numberOfRollsSoFar	Number of rolls the player has taken this turn
+	 * @param pointsSoFarThisTurn	Number of points gained so far this turn
+	 * @param pointsToGoal			Number of points from the goal
+	 * @param opponentPointsToGoal	Number of points the opponent is from the goal
+	 * @return						True if the player should roll again, false otherwise
+	 */
+	public boolean rollAgain(int numberOfRollsSoFar, int pointsSoFarThisTurn, int pointsToGoal, int opponentPointsToGoal) {
+		return false;
+	}
 }
