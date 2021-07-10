f02c05d96742108dbf49e89157e63b1a4159ffcf
Kim Weible <kw00183@my.westga.edu>
2021-07-10 16:16:48 -0500

    AdvancedStrategy - add in instance variable to calculate the average points per roll this turn, initialize to 0.0, calculate the average if number of rolls so far greater than 0 and then add in logic to the growing logic if statement that makes sure the expected number of rolls are equal between players both players still need points to win with check on average points this turn less than 7 (return true, else false)

--- src/edu/westga/cs6910/pig/model/strategies/AdvancedStrategy.java
+++ src/edu/westga/cs6910/pig/model/strategies/AdvancedStrategy.java
@@ -12,23 +12,48 @@ public class AdvancedStrategy implements PigStrategy {
 	/**
 	 * Method determines whether the player should roll again or not
 	 * 
-	 * @param numberOfRollsSoFar	Number of rolls the player has taken this turn
-	 * @param pointsSoFarThisTurn	Number of points gained so far this turn
-	 * @param pointsToGoal			Number of points from the goal
-	 * @param opponentPointsToGoal	Number of points the opponent is from the goal
-	 * @return						True if the player should roll again, false otherwise
+	 * @param numberOfRollsSoFar
+	 *            Number of rolls the player has taken this turn
+	 * @param pointsSoFarThisTurn
+	 *            Number of points gained so far this turn
+	 * @param pointsToGoal
+	 *            Number of points from the goal
+	 * @param opponentPointsToGoal
+	 *            Number of points the opponent is from the goal
+	 * @return True if the player should roll again, false otherwise
 	 */
-	public boolean rollAgain(int numberOfRollsSoFar, int pointsSoFarThisTurn, int pointsToGoal, int opponentPointsToGoal) {
-		int expectedOtherPlayerNumberOfRollsLeft = (int) Math.ceil(opponentPointsToGoal / 7);
-		int expectedPlayerNumberOfRollsLeft = (int) Math.ceil(pointsToGoal / 7); 
-		
-		if (expectedOtherPlayerNumberOfRollsLeft != expectedPlayerNumberOfRollsLeft && pointsToGoal > 0 && opponentPointsToGoal > 0) {
+	public boolean rollAgain(int numberOfRollsSoFar, int pointsSoFarThisTurn,
+			int pointsToGoal, int opponentPointsToGoal) {
+		int expectedOtherPlayerNumberOfRollsLeft = (int) Math
+				.ceil(opponentPointsToGoal / 7);
+		int expectedPlayerNumberOfRollsLeft = (int) Math.ceil(pointsToGoal / 7);
+		double averagePointsPerRollThisTurn = 0.0;
+
+		if (numberOfRollsSoFar > 0) {
+			averagePointsPerRollThisTurn = pointsSoFarThisTurn
+					/ numberOfRollsSoFar;
+		}
+
+		if (expectedOtherPlayerNumberOfRollsLeft == expectedPlayerNumberOfRollsLeft
+				&& averagePointsPerRollThisTurn < 7.0 && pointsSoFarThisTurn > 0
+				&& pointsToGoal > 0 && opponentPointsToGoal > 0) {
 			return true;
-		} else if (pointsSoFarThisTurn > 0 && pointsToGoal > 0 && opponentPointsToGoal <= 7) {
+		} else if (expectedOtherPlayerNumberOfRollsLeft == expectedPlayerNumberOfRollsLeft
+				&& averagePointsPerRollThisTurn >= 7.0 && pointsSoFarThisTurn > 0
+				&& pointsToGoal > 0 && opponentPointsToGoal > 0) {
+			return false;
+		} else if (expectedOtherPlayerNumberOfRollsLeft != expectedPlayerNumberOfRollsLeft
+				&& pointsSoFarThisTurn > 0 && pointsToGoal > 0
+				&& opponentPointsToGoal > 0) {
 			return true;
-		} else if (numberOfRollsSoFar == 0 && pointsToGoal > 0 && opponentPointsToGoal > 0) {
+		} else if (pointsSoFarThisTurn > 0 && pointsToGoal > 0
+				&& opponentPointsToGoal <= 7) {
 			return true;
-		} else if (pointsSoFarThisTurn > 0 && pointsToGoal > 0 && opponentPointsToGoal > 0) {
+		} else if (numberOfRollsSoFar == 0 && pointsToGoal > 0
+				&& opponentPointsToGoal > 0) {
+			return true;
+		} else if (pointsSoFarThisTurn > 0 && pointsToGoal > 0
+				&& opponentPointsToGoal > 0) {
 			return true;
 		}
 		return false;
