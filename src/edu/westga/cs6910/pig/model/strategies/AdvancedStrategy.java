3ce12c7fe7379e7986a947d703737294d6dcaede
Kim Weible <kw00183@my.westga.edu>
2021-07-10 13:52:24 -0500

    AdvancedStrategy - stage class file and make it implement the PigStrategy class

---
+++ src/edu/westga/cs6910/pig/model/strategies/AdvancedStrategy.java
@@ -0,0 +1,12 @@
+package edu.westga.cs6910.pig.model.strategies;
+
+/**
+ * AdvancedStrategy implements the PigStrategy class that defines the strategy
+ * algorithms.
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ */
+public abstract class AdvancedStrategy implements PigStrategy {
+
+}
