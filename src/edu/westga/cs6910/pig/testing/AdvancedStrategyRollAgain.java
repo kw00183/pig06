2539eab0f85374fd6558834a050e5b199d8fbda3
Kim Weible <kw00183@my.westga.edu>
2021-07-10 14:19:14 -0500

    AdvancedStrategyRollAgain - add in the testShouldReturnTrueAtStartOfGame JUnit test method and test that it fails on result false and passes on result true

--- src/edu/westga/cs6910/pig/testing/AdvancedStrategyRollAgain.java
+++ src/edu/westga/cs6910/pig/testing/AdvancedStrategyRollAgain.java
@@ -1,14 +1,36 @@
 package edu.westga.cs6910.pig.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AdvancedStrategyRollAgain {
+import edu.westga.cs6910.pig.model.strategies.AdvancedStrategy;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+/**
+ * This is the JUnit testing class to test the advanced strategy to roll again
+ * 
+ * @author Kim Weible
+ * @version Summer 2021
+ *
+ */
+public class AdvancedStrategyRollAgain {
+	private AdvancedStrategy strategy;
+
+	/**
+	 * run before each method execution
+	 */
+	@BeforeEach
+	public void setUp() {
+		this.strategy = new AdvancedStrategy();
 	}
 
+	/**
+	 * SUNNY-DAY Test for the first roll of the game by the computer
+	 */
+	@Test
+	public void testShouldReturnTrueAtStartOfGame() {
+		boolean result = this.strategy.rollAgain(0, 0, 100, 100);
+		assertEquals(true, result);
+	}
 }
